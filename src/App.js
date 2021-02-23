7c5acca17186dc7c4e34e92e888dfdc3ac8154f5
Aaron Blum <blumaa@gmail.com>
2021-02-23 21:16:27 +0100

    feb23

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import BumEdit from './RosaRay/BumEditWithHand'
+import MorphOnClick from './Examples/MorphOnClick'
 
 function App() {
   return (
     <div className="App">
+      <MorphOnClick />
       <BumEdit />
     </div>
   );
