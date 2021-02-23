6b1a8ecbe87e2e110d6ba0f426a32f3ab03eb739
Aaron Blum <blumaa@gmail.com>
2021-02-23 10:48:06 +0100

    int commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import BumEdit from './RosaRay/BumEdit'
+
+function App() {
+  return (
+    <div className="App">
+      <BumEdit />
+    </div>
+  );
+}
+
+export default App;
