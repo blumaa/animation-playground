55dfc53d72554c519fcb41d6ac77fbc106c66da1
Aaron Blum <a.blum@x-ion.de>
2021-02-25 17:20:05 +0100

    25 feb

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import "./App.css";
 import BumEdit from "./RosaRay/BumEditWithHand";
 import MorphOnClick from "./Examples/MorphOnClick";
+import IxiLoaderAnimation from "./xion/IxiLoader";
 
 function App() {
   return (
     <div className="gallery">
-      <div className="gallery-window">
+      {/* <div className="gallery-window">
         <MorphOnClick />
-      </div>
+      </div> */}
       <div className="gallery-window">
         <BumEdit />
+      </div>
+      <div className="gallery-window">
+        <IxiLoaderAnimation />
       </div>
     </div>
   );
